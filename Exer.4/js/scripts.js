85d4ebf2cfc33ab9dc9d778b6e0af972223bcb00
Breno Batista Silva <brenomanubs@gmail.com>
2025-06-04 22:19:39 -0300

    Exer.4_vet_completo

--- Exer.4/js/scripts.js
+++ Exer.4/js/scripts.js
@@ -1,3 +1,22 @@
 function exervet4(){
-    
+    let vet1 = []
+    let vet2 = []
+    let conta 
+    let media = 0
+
+
+    for(conta = 0; conta < 8; conta++){
+            vet1[conta] = Number(prompt(`Informe o ${conta + 1} numero desejado`))
+            media = media + vet1[conta]
+    }
+    media = media / 8
+
+    for(conta = 0; conta < 8; conta++){
+        if(vet1[conta] > media){
+            vet2.push(vet1[conta])
+        }
+    }
+    alert(`A media é: ${media}, e os numeros maiores que a media são ${vet2}`)
+
+
 }
